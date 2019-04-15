8d5f24974ac1ad5ffca8535802861859ae94eefd
Andrei Yasinski <andrewtopol856@gmail.com>
2019-04-15 20:49:27 +0300

    update menu

--- xbox/js/index.js
+++ xbox/js/index.js
@@ -1,3 +1,4 @@
 document.querySelector('#menu-btn').addEventListener('click', function(e) {
   document.querySelector('#nav').classList.toggle('menu_toggle');
+  document.querySelector('#menu-btn').classList.toggle('menu_btn_active');
 });
